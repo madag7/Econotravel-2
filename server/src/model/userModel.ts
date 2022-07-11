4a7de7a967ceb0faefbd83943036225a61a7bea4
Jurki <natsuki.rin111@gmail.com>
2022-07-11 11:32:32 +0200

    correction of the database

--- server/src/model/userModel.ts
+++ server/src/model/userModel.ts
@@ -1,11 +1,11 @@
-import { connection } from "../services/database.service";
+import  connection  from "../services/database.service";
 import { iUser, iUserLogin } from "./interface/iUser";
 
 class User {
     async saveUser(users: iUser){
         const queryStr='INSERT INTO users (nombre, email, password) VALUES ($1, $2, $3) RETURNING *'
         const values = [users.nombre, users.email, users.password] as string[]
-        const result:any = await connection(queryStr,values)
+        const result:any = await connection.query(queryStr,values)
         console.log(values)
         return result.rows[0];
        
@@ -14,13 +14,13 @@ class User {
     async getUser(users: iUserLogin){
         const queryStr = 'SELECT * FROM users WHERE email= $1'
         const values = [users.email]
-        const result:any = await connection(queryStr,values)
+        const result:any = await connection.query(queryStr,values)
         return result.rows[0];
     }
 
     async getAllUsers (){
         const queryStr = 'SELECT * FROM users';
-        const result:any = await connection(queryStr)
+        const result:any = await connection.query(queryStr)
         return result.rows;
     }
 }
