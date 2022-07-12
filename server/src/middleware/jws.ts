72671ee206a152d5701188eb28175150b11512d8
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-07-12 09:42:08 +0200

    #8 encripted login working

--- server/src/middleware/jws.ts
+++ server/src/middleware/jws.ts
@@ -6,12 +6,13 @@ const {secret} = config()
 
 const generateToken = async (req: Request, res: Response, next: NextFunction) =>{
     try{
-        const token = await jwt.sign(req.body.email, secret)
+        const token = await jwt.sign({email: req.body.email}, secret, { expiresIn: '1h', algorithm: 'HS256' })
         console.log(token)
         req.body.token = token
         
         next()
     }catch(error: any){
+        console.dir(error)
         res.status(400).send("Error al generar el token");
     }
 }
