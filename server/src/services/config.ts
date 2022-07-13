6f60519ac0d2e8a0b135c2cf3cb7ce7a0ada9103
Sara <sara13egea@hotmail.com>
2022-07-13 10:52:02 +0200

    nada

--- server/src/services/config.ts
+++ server/src/services/config.ts
@@ -3,7 +3,7 @@ import {config as dotenvConfig} from 'dotenv'
 dotenvConfig();
 
 export const config = () =>{
-    const uri= process.env.POSTGRES_URL as string;
+    const uri= process.env.DATABASE_URL as string;
     const secret= process.env.SECRET as string;
        
     return {
