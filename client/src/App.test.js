d63b4ea1987f075d713e435c467d71e1bab84613
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-07-26 16:15:20 +0200

    #13 four frontend tests and one backend test created and passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import WhoWeAre from './components/Footer/WhoWeAre';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe ('test de home',()=>{
+  beforeEach(()=>{
+    render(<App />);
+  })
+
+  test('renders App mission component', () => {
+    render(<WhoWeAre />);
+    expect(screen.getByText("Misión")).toBeInTheDocument();
+  });
+
+  test('render travel title', () => {  
+    const title = screen.getByText(/travel/i)
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('render a heading', () => {
+    const title = screen.getByRole('heading', {level: 2})
+  });
+
+  test('render a heading', () => {
+    const title = screen.getByRole('button', {name: 'Contáctanos'})
+  });
+
+
+})
